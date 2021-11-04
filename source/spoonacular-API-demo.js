8f1f97e05850069841d99e047b5c255ebcc6fd81
zhuoyu-george-tan <zhtan@ucsd.edu>
2021-11-04 12:37:12 -0700

    update api

---
+++ source/spoonacular-API-demo.js
@@ -0,0 +1,52 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    // Search recipe with key word
+    
+    let apiKey='1a75396eadf347a5ad84f4c37723d29f';
+    let id;
+
+    // apiKey: unique, link to your spoonacular id
+    // query: where you put your key words
+    fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=1a75396eadf347a5ad84f4c37723d29f&query=chicken')
+        .then(response => response.json())
+        .then(data =>{           
+            console.log(data);
+            // get the first recipe's id
+            console.log(data['results'][0]['id']);
+            //console output: 638549
+            id = data['results'][0]['id'];           
+        })
+    // get recipe by id   
+    fetch('https://api.spoonacular.com/recipes/638549/information?apiKey='+apiKey)
+        .then(response => response.json())
+        .then(data =>{           
+            console.log(data);
+            //instruction
+            console.log(data['instructions']);
+            //popular
+            console.log(data['veryPopular']);
+            //time 
+            console.log(data['readyInMinutes']);
+
+        })
+    // get instructions in detail
+    fetch('https://api.spoonacular.com/recipes/324694/analyzedInstructions?'+apiKey)
+        .then(response => response.json())
+        .then(data =>{           
+            console.log(data);
+            //instruction
+            console.log(data['instructions']);
+            //popular
+            console.log(data['veryPopular']);
+            //time 
+            console.log(data['readyInMinutes']);
+
+        })
+
+
+}
+
+
+
+
