f8c81a7249a7912b8c56f50c4f0f9e4d32d13164
Xi-Kai Wu <animal.wu@gmail.com>
2021-11-11 10:13:42 -0800

    fix for promise of fetch call

--- source/main.js
+++ source/main.js
@@ -1,15 +1,10 @@
 
 async function fetchRecipes() {
-    let recipes = []
-    fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=03722052291e4f84bce1021acd82624f&titleMatch=Garlicky Kale')
+    return fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=03722052291e4f84bce1021acd82624f&titleMatch=Garlicky Kale')
     .then(response => response.json())
-    .then(data => {   
-        for(let i = 0; i < data.results.length; i++){
-            recipes[i] = data.results[i];
-        }
-    })
-    return recipes;
 }
+let vals = await fetchRecipes();
+console.log(vals.results)
 let recipe = {
     "id": 716429,
     "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
@@ -362,11 +357,3 @@ let recipe = {
         ]
     }
 }
-
-let elementTitle = document.getElementById("title");
-elementTitle.textContent = recipe.title;
-let elementImage = document.getElementById("img");
-elementImage.src = recipe.image;
-let elementIngredients = document.getElementById("ingredients");
-elementIngredients.src = recipe.image;
-
