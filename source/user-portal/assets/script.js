a32282141a263b31bc7debac51b2beaa64741603
Brian Nhieu <bnhieu@ucsd.edu>
2021-11-18 17:41:19 -0800

    added functions for local storage

--- source/user-portal/assets/script.js
+++ source/user-portal/assets/script.js
@@ -1,9 +1,7 @@
 // Script for adding recipe card
 
-// This would be the array from Local Storage
 const recipes = [
-
-
+    'https://introweb.tech/assets/json/birthdayCake.json'
 ];
 
 const recipeData = {};
@@ -12,6 +10,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 async function init() {
 
+    
     let fetched = await fetchRecipes();
 
     if(!fetched){
@@ -19,38 +18,53 @@ async function init() {
         return;
     }
 
+    collectStorage();
+
     createRecipeCards();
 }
 
+// Function here if I need to fetch the recipe... 
+// fetch(`https://api.spoonacular.com/recipes/${recipes[i]}/information?apiKey=`+apiKey)
 async function fetchRecipes() {
     return new Promise((resolve, reject) => {
 
         for(let i = 0; i < recipes.length; i++){
             fetch(recipes[i])
-            .then((response) => response.json())
-            .then((data) => {
-                recipeData[i] = data;
-                if(i + 1 == recipes.length){
-                    resolve(true);
-                }
+                .then(response => response.json())
+                .then(data =>{           
+                    localStorage.setItem("Birthday", JSON.stringify(data));
+                    recipeData[i] = "Birthday";
+                    if(i+1 == recipes.length){
+                        resolve(true);
+                    }
             })
             .catch((error) => {
                 console.error("Error fetching: ", error);
                 reject(false);
             })
+            resolve(true);
         }
-        resolve(true);
-    });
+    })
+}
+
+function collectStorage(){
+
+    for(let i = 0; i < localStorage.length; i++){
+        recipeData[i] = localStorage.key(i);
+    }
 }
 
 function createRecipeCards(){
 
     for(let i = 0; i < 6; i++){
-
         let card = document.createElement('fav-recipe');
         
-        card.data = "Favorite more recipes!";
-
+        if(i >= localStorage.length){
+            card.data = "Favorite more recipes!";
+        }
+        else{
+            card.data = recipeData[i];
+        }
         document.querySelector("recipes").appendChild(card);
     }
 }
