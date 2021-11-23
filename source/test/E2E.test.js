e537f58c852288b3634fc6fb082f931215ffd2be
Zhuoyu Tan <zhtan@ucsd.edu>
2021-11-22 23:41:55 -0800

    added two more test

--- source/test/E2E.test.js
+++ source/test/E2E.test.js
@@ -1,82 +1,107 @@
 
 
-    
+    test('Check thumnails on home page',async () => {
+        const puppeteer = require("puppeteer");
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
+        // goto main page 
+        await page.goto("https://big-bytez.github.io/cse110-fa21-group19/source/home-page/index.html",{
+            waitUntil: 'networkidle0',//wait until all source loaded
+        });
 
-
-test('Check thumnails on home page',async () => {
-    const puppeteer = require("puppeteer");
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://big-bytez.github.io/cse110-fa21-group19/source/home-page/",{
-        waitUntil: 'networkidle0',
-      });
-    
-    //wait until the thumnails are generated
-    const numContainer = await page.$$eval(".container", (prodItems) => {
-        return prodItems.length;
-    });
-    const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
-        return prodItems.length;
-    });
-
-
-    await browser.close();
-
-    expect(numThumbnails).toBe(15);
-
-},10000);
-
-    // it("Check recipes page routing", async () => {
-
-    //     const puppeteer = require('puppeteer');
-    //     const browser = await puppeteer.launch();
-    //     const page = await browser.newPage();
-    //     await page.goto('http://127.0.0.1:5500/source/home-page/index.html');
-    //     await page.waitForSelector('#tile');
-
-    //     let checkMark = true;
-    //     setTimeout(async() => {await browser.close();},5000);
-    //     const thumnail = await page.$(".container > a");
-    //     await thumnail.click();
-
-    //     await page.waitForSelector("h1");
-    //     const title =  await page.$("h1");
-    //     let innertext = await title.getProperty("innerText");
-
-    //     if (innertext['_remoteObject'].value == ""){
-    //         checkMark = false;
-    //     }
+        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
+        //     waitUntil: 'networkidle0',//wait until all source loaded
+        // });
         
-    //     await browser.close();
-    //     expect(true).toBe(true);
+        //Grab all Thumbnails on home page
+        const numContainer = await page.$$eval(".container", (prodItems) => {
+            return prodItems.length;
+        });
+        //Grab all Containers on home page
+        const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
+            return prodItems.length;
+        });
 
 
-    // },5000);
+        await browser.close();
+        // # container * 5  = # thumnials
+        expect(numThumbnails).toBe(15);
 
 
-    // it('Check user page routing', async () => {
 
-    //     const puppeteer = require('puppeteer');
-    //     const browser = await puppeteer.launch();
-    //     const page = await browser.newPage();
-    //     await page.goto('http://127.0.0.1:5500/source/home-page/index.html');
-    //     await page.waitForSelector('#tile');
+    },10000);
 
-    //     let checkMark = true;
-    //     setTimeout(async() => {await browser.close();},5000);
-    //     const thumnail = await page.$(".user-portal > a");
-    //     await thumnail.click();
+    test('Check time button',async () => {
+        const puppeteer = require("puppeteer");
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://big-bytez.github.io/cse110-fa21-group19/source/home-page/index.html",{
+            waitUntil: 'networkidle0',
+        });
+        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
+        //     waitUntil: 'networkidle0',//wait until all source loaded
+        // });
 
-    //     await page.waitForSelector('.recipe-box');
-    //     const text =  await page.$('.recipe-box h3');
-    //     let innertext = await text.getProperty('innerText');
+        //wait untill page loaded
 
-    //     if (innertext['_remoteObject'].value == ""){
-    //         checkMark = false;
-    //     }
-    //     await browser.close();
-    //     expect(true).toBe(true);
+        // click 10 mins button
+        await page.click("center button");
+        await page.waitForNetworkIdle();
+        // await page.screenshot({
+        //     path: "./search page1.png",
+        //     fullPage: true
+        // });
 
-    // },5000);
+        // await page.screenshot({
+        // path: "./search page2.png",
+        // fullPage: true
+        // });
 
+        
+        // Grab all Thumbnails
+        const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
+            return prodItems.length;
+        });
+
+
+        await browser.close();
+
+        expect(numThumbnails > 0).toBe(true);
+
+    },20000);
+
+    test('Check search page',async () => {
+        const puppeteer = require("puppeteer");
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://big-bytez.github.io/cse110-fa21-group19/source/home-page/index.html",{
+            waitUntil: 'networkidle0',
+        });
+
+        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
+        //     waitUntil: 'networkidle0',
+        // });
+
+
+            await page.$eval('#query', el => el.value = "chicken");
+            // click 10 mins button
+            await page.click("#submit");
+            //wait untill page loaded
+            await page.waitForNetworkIdle();
+            await page.screenshot({
+                path: "./search page.png",
+                fullPage: true
+            })
+
+
+        // Grab all Thumbnails
+        const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
+            return prodItems.length;
+        });
+
+        await browser.close();
+
+        expect(numThumbnails > 0).toBe(true);
+
+    },10000);
