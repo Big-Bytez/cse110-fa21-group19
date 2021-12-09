8dc940e9f340ef772eb90ec368f8b712b1654885
Zhuoyu Tan <zhtan@ucsd.edu>
2021-12-09 13:32:17 -0800

    deleted unused functions and comments

--- source/home-page/assets/script.js
+++ source/home-page/assets/script.js
@@ -39,24 +39,7 @@
   sliders.forEach(setUpslider);
  
 
-let images = {
-    "1" : "images/pho.jfif",
-    "2" : "images/pho.jfif",
-    "3" : "images/pho.jfif",
-    "4" : "images/pho.jfif",
-    "5" : "images/pho.jfif",
-    "6" : "images/pho.jfif",
-    "7" : "images/pho.jfif",
-    "8" : "images/pho.jfif",
-    "9" : "images/pho.jfif",
-    "10" : "img/10.png"
- }
  
- /*
- Object.keys(images).forEach(function(path) {
-     $('#hold_images').append("<img class='my_img' width=200 height=400 src=" + images[path] + ">"); 
- });
- */
  function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -118,31 +101,6 @@ makeThumbnails("&titleMatch=Vegan");
 makeThumbnails("&titleMatch=Steak");
 
 
-
- 
- /*$('body').append("<i id='icon_right'></i>");
- $('body').append("<i id='icon_left'></i>"); 
- add_icon('#icon_right', 'fa fa-chevron-right', '40px', 'white');
- add_icon('#icon_left', 'fa fa-chevron-left', '40px', 'white');*/
- 
- /*
- $(document).ready(function(){
-     $('.my_img').hover(function() {
-         $(this).addClass('transition');
-     
-     }, function() {
-         $(this).removeClass('transition');
-     });
- });
- 
- $(document).on('click', '#icon_right, #icon_left', function() {
-     if($(this).attr('id') == 'icon_right') {
-         $('body').animate({scrollLeft: 1000}, 800);
-         } else {
-         $('body').animate({scrollLeft: -1000}, 800);
-     }
- });
- */
 
  document.getElementsByClassName("shuffle")[0].addEventListener('click', getRandom());
 
--- source/test/E2E.test.js
+++ source/test/E2E.test.js
@@ -10,14 +10,6 @@
             waitUntil: 'networkidle0',//wait until all source loaded
         });
 
-        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
-        //     waitUntil: 'networkidle0',//wait until all source loaded
-        // });
-        
-        //Grab all Thumbnails on home page
-        // const numContainer = await page.$$eval(".container", (prodItems) => {
-        //     return prodItems.length;
-        // });
         //Grab all Containers on home page
         const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
             return prodItems.length;
@@ -36,38 +28,20 @@
         const puppeteer = require("puppeteer");
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+        //wait untill page loaded
         await page.goto("https://big-bytez.github.io/cse110-fa21-group19/source/home-page/index.html",{
             waitUntil: 'networkidle0',
         });
-        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
-        //     waitUntil: 'networkidle0',//wait until all source loaded
-        // });
-
-        //wait untill page loaded
 
         // click 10 mins button
         await page.click("center button");
         await page.waitForNetworkIdle();
-        // await page.screenshot({
-        //     path: "./search page1.png",
-        //     fullPage: true
-        // });
-
-        // await page.screenshot({
-        // path: "./search page2.png",
-        // fullPage: true
-        // });
 
         
         // Grab all Thumbnails
         const numThumbnails = await page.$$eval('search-recipe', (prodItems) => {
             return prodItems.length;
         });
-
-    //     let checkMark = true;
-    //     setTimeout(async() => {await browser.close();},5000);
-    //     const thumnail = await page.$(".container > a");
-    //     await thumnail.click();
 
         await browser.close();
 
@@ -83,20 +57,12 @@
             waitUntil: 'networkidle0',
         });
 
-        // await page.goto("http://127.0.0.1:5500/source/home-page/index.html",{
-        //     waitUntil: 'networkidle0',
-        // });
-
 
             await page.$eval('#query', el => el.value = "chicken");
             // click 10 mins button
             await page.click("#submit");
             //wait untill page loaded
             await page.waitForNetworkIdle();
-            // await page.screenshot({
-            //     path: "./search page.png",
-            //     fullPage: true
-            // })
 
 
         // Grab all Thumbnails
